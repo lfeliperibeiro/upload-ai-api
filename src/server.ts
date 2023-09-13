2f0de454bc747012772ba22c01e28daf1ddca57c
Felipe Ribeiro <piporibeiro@gmail.com>
2023-09-13 15:46:57 -0300

    chore: add openai, dotenv and fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import { fastify } from "fastify";
+import {fastifyCors} from '@fastify/cors'
 import { getAllPromptRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAITranscriptionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.register(getAllPromptRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAITranscriptionRoute)
 
 app.listen({
   port: 3333,
